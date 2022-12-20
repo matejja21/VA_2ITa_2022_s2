50a513f214f95abd60ebb400699cbe1c963a32b6
Jan Matějíček <matejja21@sps-prosek.cz>
2022-12-20 20:35:14 +0100

    fin

--- obesenec.py
+++ obesenec.py
@@ -16,28 +16,43 @@ slova = ["auto", "kolo", "paprika"]
 while gameLoop:
     slovo = random.choice(slova)
     slovo = list(slovo)
+    pocet_pismen = 0
+    zivoty = 5
     uhadnute = []
     chybne = []
     while gameLoop:
-        print("hádaný znak")
-        pismeno = input("=>")
-        pismeno = pismeno.lower()[0]
-        check = False
-        count = 0
-        for i in slovo:
-            if pismeno == i:
-                check = True
-                count += 1
-        if check == True:
-            uhadnute.append(pismeno)
-            print(pismeno, "je v hádaném slově", count,"krát")
+        if pocet_pismen == len(slovo):
+            uhadnute_slovo = ""
+            for i in slovo:
+                uhadnute_slovo += i
+            print("Vyhrál jsi, hádané slovo bylo:", uhadnute_slovo)
+            gameLoop = False
+        elif zivoty <= 0:
+            print("Prohrál jsi")
+            gameLoop = False
         else:
-            chybne.append(pismeno)
-            print(pismeno, "se v hádaném slově nenachází")
-        nahoda = ""
-        for i in slovo:
-            if i in uhadnute:
-                nahoda += i
+            print("hádaný znak")
+            pismeno = input("=>")
+            pismeno = pismeno.lower()[0]
+            check = False
+            count = 0
+            for i in slovo:
+                if pismeno == i:
+                    check = True
+                    count += 1
+                    pocet_pismen += 1
+            if check == True:
+                uhadnute.append(pismeno)
+                print(pismeno, "je v hádaném slově", count,"krát")
             else:
-                nahoda += "_"
-        print(nahoda)
+                chybne.append(pismeno)
+                print(pismeno, "se v hádaném slově nenachází")
+                zivoty -= 1
+            nahoda = ""
+            for i in slovo:
+                if i in uhadnute:
+                    nahoda += i
+                else:
+                    nahoda += "_"
+            print(nahoda)
+            print("životy:", zivoty)
